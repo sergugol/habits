5215394a917206ade6eb0778e745cb8b653c8c10
AlexDeLaCreme <lyosha@gmail.com>
2011-11-09 03:09:40 +0200

    Sign In with Facebook & Twitter, Asset Pipeline, and robots.txt to ban all spiders

--- config/routes.rb
+++ config/routes.rb
@@ -50,6 +50,9 @@ Habits::Application.routes.draw do
   # just remember to delete public/index.html.
   root :to => "application#index"
 
+  # OmniAuth
+  match "/auth/:provider/callback" => "application#index"
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
