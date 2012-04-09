c5aba54db2033f92f89b27e10f3023ca77bcdd88
sergugol <sergugol@gmail.com>
2012-04-10 00:52:31 +0300

    one more commit

--- db/schema.rb
+++ db/schema.rb
@@ -32,13 +32,7 @@ ActiveRecord::Schema.define(:version => 20120303113811) do
     t.datetime "updated_at",  :null => false
   end
 
-  create_table "recipes", :force => true do |t|
-    t.integer  "user_id"
-    t.string   "body"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
+  
   create_table "users", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
